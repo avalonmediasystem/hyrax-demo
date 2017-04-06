c82188697803934855ca557772c633bbaf471abf
Jamie Little <jamie@jamielittle.org>
2017-04-06 11:04:50 -0400

    Add POC media player & test
    Add spec for the media player

---
+++ app/assets/javascripts/avalon/app.js
@@ -0,0 +1,9 @@
+Avalon = {
+    initialize: function () {
+    },
+
+    mediaPlayer: function(options) {
+	var MediaPlayer = require('avalon/media_player')
+        return new MediaPlayer(options)
+    }
+}
